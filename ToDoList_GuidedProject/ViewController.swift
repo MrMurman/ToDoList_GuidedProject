dd3bc0785d12fcaeb5eb10ba8ff1a4cb4e6f43f3
MrMurman <74971676+MrMurman@users.noreply.github.com>
2021-08-06 19:05:25 +0300

    Initial Commit

---
+++ ToDoList_GuidedProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList_GuidedProject
+//
+//  Created by Андрей Бородкин on 06.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
